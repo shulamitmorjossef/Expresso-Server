f527bf27a8b44579b1f34536957324ec6fcb6f5c
aderet <aderet321@gmail.com>
2025-04-27 22:23:14 +0300

    delete comments

--- tests/aboutTest.spec.js
+++ tests/aboutTest.spec.js
@@ -4,10 +4,10 @@
 
 // describe('GET /about', () => {
 //   it('should return status 200 and contain expected fields', async function () {
-//     this.timeout(5000); // נותן לבדיקה 5 שניות לסיים
+//     this.timeout(5000); // 
 //     const res = await request(app).get('/about');
     
-//     expect(res.status).to.equal(200); // כאן לשים 200!!
+//     expect(res.status).to.equal(200); // 
 //     expect(res.body).to.be.an('object');
 //     expect(res.body).to.have.property('title').that.equals('About EXpresso');
 //     expect(res.body).to.have.property('section1').that.is.a('string').that.is.not.empty;
