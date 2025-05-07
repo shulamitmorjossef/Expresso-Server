956053d35f4e70ff4ce5c0c9c1f0e283fb4d0bc9
Avishagtams <AvishTa@ac.sce.ac.il>
2025-05-07 15:16:07 +0300

    add reset password

---
+++ src/utils/passwordUtils.js
@@ -0,0 +1,10 @@
+export function generateRandomPassword(length = 10) {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      password += chars[randomIndex];
+    }
+    return password;
+  }
+  
